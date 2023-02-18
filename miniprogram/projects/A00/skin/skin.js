31de83d4bc716da15e979bc5e3f5c41bb48f81fd
zwx <zwx@qq.com>
2023-02-18 23:47:35 +0800

    CR

---
+++ miniprogram/projects/A00/skin/skin.js
@@ -0,0 +1,43 @@
+module.exports = {
+	PID: 'A00', //儿童摄影
+
+	NAV_COLOR: '#ffffff',
+	NAV_BG: '#ff5151',
+
+	MEET_NAME: '预约', 
+ 
+	MENU_ITEM: ['首页', '预约日历', '我的'], // 第1,4,5菜单
+
+	NEWS_CATE: '1=本店动态,2=样片欣赏,3=套系介绍',
+	MEET_TYPE: '1=拍摄预约|leftbig3',
+
+	DEFAULT_FORMS: [{
+			type: 'line',
+			title: '姓名',
+			desc: '请填写您的姓名',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		},
+		{
+			type: 'line',
+			title: '手机',
+			desc: '请填写您的手机号码',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		}
+	]
+}
